74881b76a944fab6d590e4b7b1e8219d84d71e8e
shweta-angularminds <shweta@angularminds.in>
2025-06-13 12:02:59 +0530

    fix: load environment variables before usage

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-const app = require("./app.js");
-const dotenv = require('dotenv');
-
+const dotenv = require("dotenv");
 dotenv.config();
+
+const app = require("./app.js");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Sever running on http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Sever running on http://localhost:${PORT}`);
 });
